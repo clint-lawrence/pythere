1d9c020e8162eb8e63547d55cda79a7751bf1883
Clint Lawrence <clint.lawrence@gmail.com>
2020-06-21 19:59:06 +1000

    Setup pre-commit and reformat script with black.

--- setup.py
+++ setup.py
@@ -11,19 +11,14 @@ setup(
     license="LICENSE",
     description="pythere is a tool to run python scripts over ssh",
     long_description=open("README.md").read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "fabric",
-    ],
+    long_description_content_type="text/markdown",
+    install_requires=["fabric",],
+    extras_require={"dev": ["pre-commit", "black"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={
-        "console_scripts": [
-            "pythere=pythere.__main__:main",
-        ],
-    }
+    entry_points={"console_scripts": ["pythere=pythere.__main__:main",],},
 )
